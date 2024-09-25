5e11a7bc4a394ebe5a32773ff423e3609bae478a
dsmercy <dsmercy9@gmail.com>
2024-09-25 12:27:09 +0530

    Add project files.

---
+++ Kafka.ConsumerTwo/Program.cs
@@ -0,0 +1,48 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Threading;
+
+namespace Kafka.ConsumerTwo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Consumer 2 started");
+            string brokerList = "localhost:9092";
+            string topicName = "test-topic";
+
+            var config = new ConsumerConfig
+            {
+                GroupId = "consumer-group-2",
+                BootstrapServers = brokerList,
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            using (var consumer = new ConsumerBuilder<Null, string>(config).Build())
+            {
+                consumer.Subscribe(topicName);
+                CancellationTokenSource cts = new CancellationTokenSource();
+                Console.CancelKeyPress += (_, e) =>
+                {
+                    e.Cancel = true;
+                    cts.Cancel();
+                };
+
+                try
+                {
+                    while (true)
+                    {
+                        var consumeResult = consumer.Consume(cts.Token);
+                        Console.WriteLine($"Consumer 2: {consumeResult.Message.Value}");
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    consumer.Close();
+                }
+                Console.WriteLine($"Consumer 2 completed");
+            }
+        }
+    }
+}
---
+++ Kafka.Producer/Program.cs
@@ -0,0 +1,39 @@
+﻿using Confluent.Kafka;
+using System;
+
+namespace Kafka.Producer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Producer started.");
+            string brokerList = "localhost:9092";
+            string topicName = "test-topic";
+
+            var config = new ProducerConfig { BootstrapServers = brokerList };
+
+            using (var producer = new ProducerBuilder<Null, string>(config).Build())
+            {
+                while (true)
+                {
+                    Console.WriteLine("Enter a message to produce (or type 'exit' to quit):");
+                    string inputMessage = Console.ReadLine();
+
+                    if (inputMessage.ToLower() == "exit")
+                    {
+                        break;
+                    }
+
+                    producer.Produce(topicName, new Message<Null, string> { Value = inputMessage });
+                    Console.WriteLine($"Produced: {inputMessage}");
+                }
+
+                // Ensure all outstanding messages are delivered
+                producer.Flush(TimeSpan.FromSeconds(10));
+            }
+
+            Console.WriteLine("Producer completed.");
+        }
+    }
+}
